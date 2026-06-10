a7adceba80a88f3bdfe6227df6bba53efeb75dc8
kael1331 <kearcangel@gmail.com>
2026-06-10 18:49:29 -0300

    fix: importar AuthModule en PlatformSettingsModule para resolver dependencias de AuthGuard

--- user-api/src/platform-settings/platform-settings.module.ts
+++ user-api/src/platform-settings/platform-settings.module.ts
@@ -3,9 +3,13 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 import { PlatformSettings } from './entities/platform-settings.entity';
 import { PlatformSettingsService } from './platform-settings.service';
 import { PlatformSettingsController } from './platform-settings.controller';
+import { AuthModule } from '../auth/auth.module';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([PlatformSettings])],
+  imports: [
+    TypeOrmModule.forFeature([PlatformSettings]),
+    AuthModule,
+  ],
   controllers: [PlatformSettingsController],
   providers: [PlatformSettingsService],
   exports: [TypeOrmModule, PlatformSettingsService],
